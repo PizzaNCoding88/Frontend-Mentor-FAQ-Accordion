2b5641f72c4eaa679ec374f9cb7aecabfb558016
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2022-10-15 17:46:33 +0100

    0.22

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const arrowBtn = document.querySelectorAll(".arrow");
 const legendTxt = document.querySelectorAll(".faq");
 const collapseBtn = document.querySelector(".collapse");
+const faqTitle = document.querySelectorAll(".faq-title")
 
 //ALTERNATIVE WITH FOR EACH
 // arrowBtn.forEach((btn,index) => {
@@ -15,11 +16,17 @@ for (let i=0 ; i < arrowBtn.length ; i++){
     arrowBtn[i].addEventListener("click" , function(){
         // console.log(`Arrow No ${i+1} clicked`);
         legendTxt[i].classList.toggle("hidden");
+        if(!legendTxt[i].classList.contains("hidden")){
+            faqTitle[i].classList.add("bold");
+        } else{
+            faqTitle[i].classList.remove("bold");
+        }
     })
 }
 
 collapseBtn.addEventListener("click" , function(){
     for (let i=0 ; i < arrowBtn.length ; i++){
         legendTxt[i].classList.add("hidden");
+        faqTitle[i].classList.remove("bold");
     }
 });
