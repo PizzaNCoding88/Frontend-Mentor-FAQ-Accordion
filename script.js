5f15628fc8a32d7e0ea6c290f6f9124b77695853
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2022-10-20 21:35:14 +0100

    0.31

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 //GRABBERS
 
-const arrowBtn = document.querySelectorAll(".arrow");
-const legendTxt = document.querySelectorAll(".faq");
-const collapseBtn = document.querySelector(".collapse");
-const faqTitle = document.querySelectorAll(".faq-title")
 
-//ALTERNATIVE WITH FOR EACH
-// arrowBtn.forEach((btn,index) => {
-//     btn.addEventListener("click" , function(){
-//         console.log(`Arrow No ${index+1} clicked`);
+//OLD
+// const arrowBtn = document.querySelectorAll(".arrow");
+// const faqTitle = document.querySelectorAll(".faq-title")
+// const legendTxt = document.querySelectorAll(".faq");
+// const collapseBtn = document.querySelector(".collapse");
+
+// //ALTERNATIVE WITH FOR EACH
+// // arrowBtn.forEach((btn,index) => {
+// //     btn.addEventListener("click" , function(){
+// //         console.log(`Arrow No ${index+1} clicked`);
+// //     })
+// // })
+
+// for (let i=0 ; i < arrowBtn.length ; i++){
+//     faqTitle[i].addEventListener("click", function(){
+//         legendTxt[i].classList.toggle("hidden");
+//         if(!legendTxt[i].classList.contains("hidden")){
+//             faqTitle[i].classList.add("bold");
+//         } else{
+//             faqTitle[i].classList.remove("bold");
+//         }
 //     })
-// })
+// };
 
-for (let i=0 ; i < arrowBtn.length ; i++){
-    arrowBtn[i].addEventListener("click" , function(){
-        // console.log(`Arrow No ${i+1} clicked`);
-        legendTxt[i].classList.toggle("hidden");
-        if(!legendTxt[i].classList.contains("hidden")){
-            faqTitle[i].classList.add("bold");
-        } else{
-            faqTitle[i].classList.remove("bold");
-        }
+// collapseBtn.addEventListener("click" , function(){
+//     for (let i=0 ; i < arrowBtn.length ; i++){
+//         legendTxt[i].classList.add("hidden");
+//         faqTitle[i].classList.remove("bold");
+//     }
+// });
+
+const faqTitles = document.querySelectorAll(".faq-title");
+const faqText = document.querySelectorAll(".faq");
+
+for(let i=0; i<faqTitles.length ; i++){    
+    faqTitles[i].addEventListener("click", function(){
+        faqText[i].classList.toggle("hidden");
+        for(let j=0; j<faqText.length;j++){
+            if(j!=i){
+                faqText[j].classList.add("hidden");
+                faqTitles[j].classList.remove("bold");
+                faqTitles[i].classList.add("bold");
+                if(faqText[i].classList.contains("hidden")){
+                    faqTitles[i].classList.remove("bold");
+                }
+            }
+         }
     })
-}
+};
 
-collapseBtn.addEventListener("click" , function(){
-    for (let i=0 ; i < arrowBtn.length ; i++){
-        legendTxt[i].classList.add("hidden");
-        faqTitle[i].classList.remove("bold");
-    }
-});
+
